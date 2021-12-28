58ba90fdfdb36cca6a1c4b0fc652b14dbb10f7c1
Yvan Brunel <8brunel@gmail.com>
2021-12-28 23:33:52 +0100

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -5,15 +5,15 @@
 #####################################################################
 
 ############## OR USING A STRING OF TEXT 
-texts ="Business owners across the country, built hotels and residencies to serve the accommodation needs of a wide range of persons from travelers to family members and even young students. At the beginning, these inns only offered basic services, but as time went by and demand for short-term accommodation grew, the hotel owners became prepared to provide guests with more facilities. Since then, the hotel industry expanded further as people were willing to pay more for better quality services. This industry has always striven to fulfil the changing demands of the society and has developed into an essential part of the economy. Today, travelers can find different types of accommodation based on their budget, from cheap bed and breakfasts, to luxurious 5-star hotels. Hotel Management can be defined as the organizing, monitoring and controlling of resources in order to ensure the running of a hotel in any due time." 
+texts ="Business,owners across the country, built hotels and residencies to serve the accommodation needs of a wide range of persons from travelers to family members and even young students. At the beginning, these inns only offered basic services, but as time went by and demand for short-term accommodation grew, the hotel owners became prepared to provide guests with more facilities. Since then, the hotel industry expanded further as people were willing to pay more for better quality services. This industry has always striven to fulfil the changing demands of the society and has developed into an essential part of the economy. Today, travelers can find different types of accommodation based on their budget, from cheap bed and breakfasts, to luxurious 5-star hotels. Hotel Management can be defined as the organizing, monitoring and controlling of resources in order to ensure the running of a hotel in any due time." 
 
 ############## HERE every single special characters are replaced with NOTHING
 
-replaced_text = texts.replace(",","")
+replaced_text = texts.replace(","," ")
 
-replaced_text = replaced_text.replace(".","")
+replaced_text = replaced_text.replace("."," ")
 
-replaced_text = replaced_text.replace("-","")
+replaced_text = replaced_text.replace("-"," ")
 
 print(replaced_text)
 
